d41d3916d9938c7a9adcc08ae50b01af049b5e3b
Denis Sorrilha <denis.sorrilha@gmail.com>
2021-08-17 20:29:29 -0300

    Edicao de usuarios funcionando

--- administracao/forms/usuario_forms.py
+++ administracao/forms/usuario_forms.py
@@ -1,10 +1,10 @@
 
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.forms import UserCreationForm
 
-class UsuarioForm(UserCreationForm):
+class CadastroUsuarioForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'first_name', 'email', 'password1', 'password2']
@@ -16,3 +16,10 @@ class UsuarioForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class EditarUsuarioForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
